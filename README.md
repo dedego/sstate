b77cf7e4d2127ffdd9df361db855bc4a79a1e67e
Dennis de Goede <dennis.de.goede@ing.com>
2019-06-18 16:26:28 +0200

    Added initial readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# sshtate
+# Sshtate
 
+Sshtate is a simplified take on state management. The library has the benefit that you can listen to changes for specific parts
+of the state. The example below, gives you a bit of an idea how it works.
+
+```
+import { Sshtate } from 'sshtate';
+
+const initialState = { bread: { baguette: 2, loaf: 6 }, fruit: { apples: 11, bananas: 1 } };
+const FoodStore = new Sshtate( initialState );
+
+FoodStore.subscribe('subscriptionId', 'bread.loaf', (newValue, previousValue) => {
+    console.log(newValue, previousValue);
+});
+
+FoodStore.setState('bread.loaf', 22); // Will log 22 as the new value and 6 as the old
+FoodStore.setState('bread.loaf', 24); // Will log 24 as the new value and 22 as the old
+
+// Will log the complete state
+console.log( FoodStore.getState() );
+// Will log the specific part of the state
+console.log( FoodStore.getState('fruit.apples') );
+
+setTimeout(() => {
+    // Will remove the subscription based on the subscription ID and the given path.
+    FoodStore.unsubscribe('subscriptionId', 'bread.loaf') 
+}, 10000);
+```
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@
     let storeEl;
     let Listeners = {};
 
+    /**
+     * Helper method to get the value of a nested object property.
+     * @param {*} root 
+     * @param {*} key 
+     */
     const get = (root, key) => key.split('.').reduce((acc, current) => acc[current], root);
+
+    /**
+     * Helper method to set value of a nested object property.
+     * @param {*} root 
+     * @param {*} key 
+     * @param {*} val 
+     */
     const set = (root, key, val) => {
         const keyParts = key.split('.');
         const length = keyParts.length;
@@ -14,13 +26,24 @@
         }
         root[keyParts[length - 1]] = val;
     };
+
+    /**
+     * Helper method for adding/removing the eventListener for your subscription(s).
+     * @param {*} storeEl 
+     * @param {*} unique 
+     * @param {*} key 
+     * @param {*} cb 
+     */
     const ev = (storeEl, unique, key, cb) => {
-        const callback = ({ detail }) => cb(detail);
+        const callback = ({ detail }) => cb(detail.new, detail.previous);
         if (cb) Listeners[unique] = callback;
         storeEl[!cb ? 'removeEventListener' : 'addEventListener'](`Sshtate.${key}`, !cb ? Listeners[unique] : callback);
         if (!cb) delete Listeners[unique];
     }
 
+    /**
+     * Sshtate simplified store
+     */
     class Sshtate {
         constructor(initialState) {
             storeEl = document.createElement('meta');
@@ -32,7 +55,7 @@
                 set(state, key, val);
                 storeEl.dispatchEvent(new CustomEvent(`Sshtate.${key}`, {
                     bubbles: true,
-                    detail: val
+                    detail: { new: val, previous: currentState }
                 }));
             }
         }
